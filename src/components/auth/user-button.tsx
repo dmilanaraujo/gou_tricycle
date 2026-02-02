0bc9190cfab0787bc5fa2240668ad2c398a3842a
dalo <daloinvest@gmail.com>
2026-02-02 18:19:07 -0500

    update user button

--- src/components/auth/user-button.tsx
+++ src/components/auth/user-button.tsx
@@ -6,11 +6,12 @@ import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, 
 import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar';
 import {LogoutButton} from '@/components/auth/logout-button';
 import {ChevronsUpDown} from 'lucide-react';
+import {getInitials, getPublicBusinessImageUrl} from '@/lib/utils';
+import {getProfileCachedData} from '@/lib/actions/profile';
 
 export async function UserButton() {
-    const supabase = await createClient();
-    const {data: {user}, error} = await supabase.auth.getUser();
-    if (!user) {
+    const profile = await getProfileCachedData();
+    if (!profile) {
         return (
             <Button asChild>
                 <Link href='/me'>
@@ -27,9 +28,8 @@ export async function UserButton() {
                     className="flex items-center"
                 >
                     <Avatar className="h-8 w-8 rounded-lg">
-                        {/*<AvatarImage src={avatar} alt={fullName} />*/}
-                        {/*<AvatarFallback className="rounded-lg">{avatarFallback}</AvatarFallback>*/}
-                        <AvatarFallback className="rounded-lg">{'NA'}</AvatarFallback>
+                        <AvatarImage src={getPublicBusinessImageUrl(profile.logo)} alt={profile.name} />
+                        <AvatarFallback className="rounded-lg">{getInitials(profile.name)}</AvatarFallback>
                     </Avatar>
                     {/*<div className="grid flex-1 text-left text-sm leading-tight">*/}
                     {/*    /!*<span className="truncate font-semibold">{fullName}</span>*!/*/}
@@ -53,9 +53,8 @@ export async function UserButton() {
                         {/*    <AvatarFallback className="rounded-lg">{'NA'}</AvatarFallback>*/}
                         {/*</Avatar>*/}
                         <div className="grid flex-1 text-left text-sm leading-tight">
-                            {/*<span className="truncate font-semibold">{fullName}</span>*/}
-                            <span className="truncate font-semibold">{'Name'}</span>
-                            <span className="truncate text-xs">{user.phone}</span>
+                            <span className="truncate font-semibold">{profile.name}</span>
+                            <span className="truncate text-xs">{profile.phone}</span>
                         </div>
                     </div>
                 </DropdownMenuLabel>
